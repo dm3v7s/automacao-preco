84e489a80a60430a96c027f8f4a287961929f3e7
Domingos <dmgvns@hotmail.com>
2022-07-06 19:44:07 -0300

    Atualização

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 from google_shopping import busca_google
 from americanas import americanas
+import pandas as pd
 
-print(americanas('tv', 'vex', 4000, 100))
-print(busca_google('tv', 'vex', 4000, 100))
+# importando a base de informações
+lista = pd.read_excel("lista.xlsx")
+
+# Criando a tabela
+tabela_ofertas = pd.DataFrame()
+
+for linha in lista.index:
+    produto = lista.loc[linha, "Nome"]
+    termos_banidos = lista.loc[linha, "Termos banidos"]
+    preco_min = lista.loc[linha, "Preço mínimo"]
+    preco_max = lista.loc[linha, "Preço máximo"]
+
+
+
+    lista_ofertas_google = busca_google(produto, termos_banidos, preco_max, preco_min)
+    if lista_ofertas_google:
+        tabela_google = pd.DataFrame(lista_ofertas_google, columns=['nome', 'preco', 'link'])
+    else:
+        tabela_google = None
+
+    lista_ofertas_americanas = americanas(produto, termos_banidos, preco_max, preco_min)
+    if lista_ofertas_americanas:
+        tabela_americanas = pd.DataFrame(lista_ofertas_americanas, columns=['nome', 'preco', 'link'])
+    else:
+        tabela_americanas = None
+
+    display(tabela_ofertas)
+
